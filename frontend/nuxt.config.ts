52fed74e41432a2fe0c24c03b4a9b6f06eb2bbea
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-05-01 02:04:51 +0800

    Update nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
     ],
   },
   css: ['~/assets/css/main.css'],
+  build: {
+    transpile: ['lucide-vue-next']
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
