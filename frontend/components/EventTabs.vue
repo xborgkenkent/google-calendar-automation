fe144674e1f3c750f7e940618ee8aa26d775891d
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-05-01 12:48:09 +0800

    Update event tabs

--- frontend/components/EventTabs.vue
+++ frontend/components/EventTabs.vue
@@ -10,10 +10,10 @@
 </template>
 
 <script setup>
-const props = defineProps({ activeTab: String });
+const props = defineProps({ activeTab: String })
 const tabClass = (tab) => {
   return `flex-1 py-4 px-6 text-center font-medium ${
       props.activeTab === tab ? 'text-blue-600 border-b-2 border-blue-600' : 'text-gray-500 hover:text-gray-700'
-  }`;
-};
+  }`
+}
 </script>
