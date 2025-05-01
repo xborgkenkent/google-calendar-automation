dda180b24e391d4e4d0a2d9361143628bbeaada2
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-05-01 12:49:01 +0800

    Update event types

--- frontend/types/Event.ts
+++ frontend/types/Event.ts
@@ -1,14 +1,14 @@
 export interface CalendarEvent {
-    id: string;
-    summary: string;
-    description?: string;
-    location?: string;
+    id: string
+    summary: string
+    description?: string
+    location?: string
     start: {
-        dateTime: string;
-        timeZone: string;
-    };
+        dateTime: string
+        timeZone: string
+    }
     end: {
-        dateTime: string;
-        timeZone: string;
-    };
+        dateTime: string
+        timeZone: string
+    }
 }
