38f183b29d40d54c34de04d892fe286395282a44
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-05-01 12:49:10 +0800

    Update dateTime utils

--- frontend/utils/dateTime.ts
+++ frontend/utils/dateTime.ts
@@ -1,4 +1,4 @@
-export const formatDate = (dateTime) => new Date(dateTime).toLocaleDateString();
-export const formatTime = (dateTime) => new Date(dateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+export const formatDate = (dateTime: string) => new Date(dateTime).toLocaleDateString()
+export const formatTime = (dateTime: string) => new Date(dateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
 
-export const generateDateTime = (date, time) => `${date}T${time}:00`;
+export const generateDateTime = (date: string, time: string) => `${date}T${time}:00`
