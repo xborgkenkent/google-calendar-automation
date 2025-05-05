6d78256442bcbad831d6b7dc276ac1775dd01ae9
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-05-05 16:22:35 +0800

    update openai routes

--- backend/api/openai_routes.py
+++ backend/api/openai_routes.py
@@ -7,62 +7,18 @@ import os
 from openai import OpenAI
 from services.google_services import create_calendar_event
 from models.openai_models import EventRequest
-
+from services.openai_services import generate_event_from_text
 load_dotenv()
 
 router = APIRouter()
 api_key = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
-DEFAULT_TIMEZONE = "Asia/Manila"
-
-def transform_to_calendar_format(raw_data: dict) -> dict:
-    return {
-        "summary": raw_data.get("summary", ""),
-        "start": {
-            "dateTime": raw_data.get("start", ""),
-            "timeZone": DEFAULT_TIMEZONE
-        },
-        "end": {
-            "dateTime": raw_data.get("end", ""),
-            "timeZone": DEFAULT_TIMEZONE
-        },
-        "description": raw_data.get("description", ""),
-        "location": raw_data.get("location", "")
-    }
-    
 @router.post("/generates")
 async def generate_event(event: EventRequest = Body(...)):
     """Get event summary"""
     try:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {
-                    "role": "assistant",
-                    "content": (
-                        "You are a google calendar assistant. JSON output should be: "
-                        "{\"summary\": \"the event title\"}, "
-                        "{\"description\": \"the event description\"}, "
-                        "{\"start\": \"the start time\" //ex: 2025-05-02T21:00:00+08:00 }, "
-                        "{\"end\": \"the end time\" //ex: 2025-05-02T23:00:00+08:00}, "
-                        "{\"location\": \"the event location\"}"
-                    )
-                },
-                {"role": "user", "content": event.val},
-            ],
-        )
-        content = response.choices[0].message.content
-
-        match = re.search(r"```json\s*({[\s\S]*?})\s*```", content)
-        if not match:
-            match = re.search(r"({[\s\S]*?})", content)
-        
-        if not match:
-            raise ValueError("JSON object not found in response.")
-
-        extracted_json = json.loads(match.group(1))
-        transformed = transform_to_calendar_format(extracted_json)
+        transformed = await generate_event_from_text(event.val)
         create_calendar_event(transformed)
         return transformed
 
